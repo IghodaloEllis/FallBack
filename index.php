1eed190558806baced94861c6621544779df8350
Ighodalo Ellis <32099998+IghodaloEllis@users.noreply.github.com>
2024-08-16 08:42:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-html
-<!DOCTYPE html>
-<html>
-<head>
-<title>Login</title>
-</head>
-<body>
-<h2>Login</h2>
-<form action="authenticate.php" method="post">
-<label for="email">Email:</label>
-<input type="email" id="email" name="email" required><br><br>
-<label for="password">Password:</label> 1 
-<input type="password" id="password" name="password" required><br><br>
-<button type="submit">Login</button>
-</form>
-</body>
-</html> 2   
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+// Rest of your script
+?>
