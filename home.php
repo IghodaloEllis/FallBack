3f9003f73625733c27598c2c57778285de461c9e
Ighodalo Ellis <32099998+IghodaloEllis@users.noreply.github.com>
2024-08-16 11:06:22 +0100

    Update home.php

--- home.php
+++ home.php
@@ -7,6 +7,10 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
-
+// Display user-specific message. You can customise this part
+echo "Welcome, " . $_SESSION['username'] . "!";
 // Rest of your script
+// You can print whatever you like below or comment them out.
+echo "<p>User-generated content: " . htmlspecialchars($user_input) . "</p>";
+
 ?>
