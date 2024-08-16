d507f912b2f4a2d3680b84b921d0f9413074bc79
Ighodalo Ellis <32099998+IghodaloEllis@users.noreply.github.com>
2024-08-16 08:41:40 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+<h2>Login</h2>
+<form action="authenticate.php" method="post">
+<label for="email">Email:</label>
+<input type="email" id="email" name="email" required><br><br>
+<label for="password">Password:</label> 1 
+<input type="password" id="password" name="password" required><br><br>
+<button type="submit">Login</button>
+</form>
+</body>
+</html> 2   
